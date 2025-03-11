ee6abbc3dceab80a4982e3f3bc7c16177647cdf4
fahrezacode <ahmadfahreza.@students.amikom.ac.id>
2025-03-11 14:18:37 +0700

    penambahan dan pengurangan

--- calculator.cs
+++ calculator.cs
@@ -5,3 +5,22 @@ class Calculator
 		
 	}
 }
+{
+return a + b;
+
+ConsoleWriteLine("Hasil penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+ConsoleWriteLine("Hasil Pengurangan: {0} - {1} = {2}",a, b, Pengurangan(a, b));
+
+ConsoleWriteLine("\Tekan sembarang untuk key keluar");
+Console.Readkey();
+}
+
+static int Penambahan (int a, b)
+{
+	return a + b;
+}
+
+static int Pengurangan(int a, int b)
+{
+	return a - b;
+}
